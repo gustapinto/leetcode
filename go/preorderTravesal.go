99d876f69ee236fca862cfba4f87e8550f05a7c0
Gustavo Henrique Pinto <ghp2201@gmail.com>
2022-01-30 08:18:01 -0300

    Preorder Binary Tree Travesal

---
+++ go/preorderTravesal.go
@@ -0,0 +1,25 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func preorderTraversal(root *TreeNode) []int {
+	result := make([]int, 0)
+
+	var preorder func(root *TreeNode, carry []int)
+	preorder = func(root *TreeNode, carry []int) {
+		if root != nil {
+			result = append(carry, root.Val)
+
+			preorder(root.Left, result)
+			preorder(root.Right, result)
+		}
+	}
+
+	preorder(root, []int{})
+
+	return result
+}
